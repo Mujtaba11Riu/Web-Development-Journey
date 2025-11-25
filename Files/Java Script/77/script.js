495a756dcd012427acb7f58232f5c0394077fe70
Mujtaba11Riu <syedmujtaba773@gmail.com>
2025-11-25 20:43:44 +0500

    in progress

--- Files/Java Script/77/script.js
+++ Files/Java Script/77/script.js
@@ -1,5 +1,41 @@
+
+
+
 function createCard(title, cName, views, monthsOld, duration, thumbnail){
     // Finish this function
+  
+  
+    let viewStr
+
+
+    if (views<1000000){
+
+  viewStr= views/1000 + "K";
+     
+    }
+
+    else if (views>1000000){
+        
+      viewStr = views/1000000 + "M";
+        }
+
+        else {
+          viewStr=views/1000 + "K";
+        }
+
+
+
+
+    let html = `  <div class="card">
+            <div class="image">
+                <img src="${thumbnail}" alt="">
+            </div>
+            <div class="text">
+                <h1>${title}</h1>
+                <p>${cName} . ${viewStr} views. ${monthsOld} ago</p>
+            </div>
+
+        </div>`
 }
 
 
