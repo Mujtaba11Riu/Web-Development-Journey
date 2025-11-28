3dc6dbad55456607b94ffe7f6c9c292e1b515808
Mujtaba11Riu <syedmujtaba773@gmail.com>
2025-11-28 22:41:21 +0500

    error cathch

--- Files/Java Script/78/script.js
+++ Files/Java Script/78/script.js
@@ -5,6 +5,11 @@ let a = prompt("Enter First Number ")
 let b = prompt("Enter Second Number ")
 
 // let sum = a + b
+
+if(isNaN (a) || isNaN(b)){
+    throw SyntaxError("Sorry This Is Not Allowed")
+}
+
 let sum = parseInt(a) + parseInt(b)
 
 console.log("The Sum is ", sum)
