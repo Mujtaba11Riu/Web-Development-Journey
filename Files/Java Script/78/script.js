b09df70cb5c5dbabd47ef84e5d37f5799b71dd6a
Mujtaba11Riu <syedmujtaba773@gmail.com>
2025-11-28 22:35:10 +0500

    ok

--- Files/Java Script/78/script.js
+++ Files/Java Script/78/script.js
@@ -4,7 +4,8 @@ let a = prompt("Enter First Number ")
 
 let b = prompt("Enter Second Number ")
 
-let sum = a + b
+// let sum = a + b
+let sum = parseInt(a) + parseInt(b)
 
 console.log("The Sum is ", sum)
 
