7eeef095a3f17c407cf1e11930babd738fe61feb
Mujtaba11Riu <syedmujtaba773@gmail.com>
2025-12-04 22:14:20 +0500

    super done

--- Files/Java Script/80/script.js
+++ Files/Java Script/80/script.js
@@ -1,6 +1,57 @@
-let obj = {
-    a:1,
-    b:"Harry"
+// let obj = {
+//     a:1,
+//     b:"Harry"
+// }
+
+// console.log(obj)
+
+// let animal = {
+//     eats: true
+// };
+
+// let rabbit = {
+//     jumps: true
+// };
+
+// rabbit._proto_ = animal; 
+
+
+class Animal {
+    
+    constructor(name){
+
+        this.name=name
+        console.log("Object is created...")
+    }
+
+    eats(){
+        console.log("I am eating")
+    }
+    jumps (){
+        console.log("I am Jumping ")
+    }
+
 }
 
-console.log(obj)
+class Lion extends Animal{
+    constructor(name){
+        super(name)
+        this.name=name
+        console.log("Object is created...He is loin ")
+    }
+
+ eats(){
+        console.log("I am drinking")
+    }
+
+
+} 
+
+
+let a = new Animal("Motii");
+console.log(a)
+
+
+
+let l = new Lion ("Donkey ");
+console.log(l)
